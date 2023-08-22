935d898da97afda1232f6d580d95372b12d977b7
jfurches <jim.furches@gmail.com>
2023-08-22 17:18:42 -0400

    Do constrained nonviolation hamiltonian

--- src/nonlocalgames/util.py
+++ src/nonlocalgames/util.py
@@ -1,5 +1,17 @@
-from typing import Dict, TypeVar, Tuple
 from functools import partial
+from typing import Dict, Tuple, TypeVar
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import pandas as pd
+from matplotlib.cm import get_cmap
+from matplotlib.colors import Colormap
+from matplotlib.collections import LineCollection, PathCollection
+
+from nonlocalgames.hamiltonians import G14
+
 
 def load_seeds(path = 'data/seeds.txt'):
     '''Loads some random seeds'''
@@ -32,5 +44,49 @@ def from_ket_form(counts: Dict[str, T]) -> Dict[Tuple[int], T]:
         # with the MSB on the left.
         answers = tuple(map(from_bin, reversed(bitstring.split(' '))))
         postprocessed[answers] = count
-    
+
     return postprocessed
+
+def draw_graph(G: nx.Graph, cmap: str | Colormap = 'PiYG'):
+    '''Draws a graph and colors the nodes and vertices based on the `weight` attribute.
+    
+    Args:
+        G: Graph to draw. Both nodes and vertices should have a `weight` attribute
+        
+        cmap: The colormap to use. Can be a matplotlib string or a Colormap instance.
+    
+    Returns:
+        The Axes of the new figure
+    '''
+    if isinstance(cmap, str):
+        cmap = get_cmap(cmap)
+
+    node_colors = list(map(lambda it: it[-1], G.nodes.data('weight')))
+    edge_colors = list(map(lambda it: it[-1], G.edges.data('weight')))
+
+    vmin, vmax = min([*node_colors, *edge_colors]), max([*node_colors, *edge_colors])
+
+    pos = nx.kamada_kawai_layout(G)
+    nodes: PathCollection = nx.draw_networkx_nodes(
+        G,
+        pos,
+        node_color='white',
+        edgecolors=cmap(node_colors),
+        node_size=500,
+    )
+    edges: LineCollection = nx.draw_networkx_edges(
+        G,
+        pos,
+        edge_color=edge_colors,
+        edge_cmap=cmap,
+        edge_vmin=vmin,
+        edge_vmax=vmax,
+        width=2,
+    )
+    nx.draw_networkx_labels(G, pos, {n: str(n) for n in G}, font_color="black")
+    edges.set_array(edge_colors)
+
+    ax = plt.gca()
+    ax.set_axis_off()
+    plt.colorbar(edges, ax=ax)
+    return ax
